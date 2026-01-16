fc21d90bb01752a48eacef51110341e758fa03cc
Vinay M S <vvvinayms99@gmail.com>
2026-01-16 01:41:19 +0000

    commit

--- macros/mcr_acct_percentage.sql
+++ macros/mcr_acct_percentage.sql
@@ -1,5 +1,5 @@
 {% macro m_acct_percentage(c1,c2) %}
-    case when {{c1}} ='USA' then {{c2}}*0.3
+    case when {{c1}} ='USA' then {{c2}}*0.3 -- here c1 is country column and c2 is tax column - meaning if country is USA then tax is 30%
          when {{c1}} = 'INDIA' then {{c2}}*0.2
          else {{c2}}*0.5 end new_balance
 {% endmacro %}
